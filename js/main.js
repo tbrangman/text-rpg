104d9b51f3519643498ad042e8a3b3ed690d264d
tbrangman <tylerbrangman1@pal-nat186-116-62.itap.purdue.edu>
2016-12-01 13:01:33 -0500

    Building blocks
    HTML, CSS, Grid, and linking of files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function (e) {
+	"use strict";
+	
+	$(".more-info a").click(function () {
+     $("#more-info").toggleClass("display", "block");
+   });
+	
+	var name, race, className;
+});
