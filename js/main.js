49a4c025ee11247aedccdc7e4a4399e9dc61b937
tbrangman <tylerbrangman1@pal-nat186-114-236.itap.purdue.edu>
2016-12-03 15:25:45 -0500

    class-select page

--- js/main.js
+++ js/main.js
@@ -2,9 +2,19 @@ $(document).ready(function (e) {
 	"use strict";
 	
 	$(".more-info a").click(function () {
-     $("#about").css("display", "block");
-			$("#info-close").css("display", "none");
+    $("#about").css("display", "block");
+		$("#info-close").css("display", "block");
    });
+	
+	$("#info-close").click(function () {
+		$("#about").css("display", "none");
+		$("#info-close").css("display", "none");
+   });
+	
+	function NameofThanos() {
+		var x = document.getElementById("name-box").value;
+		document.getElementById("display-name").innerHTML = x;
+	}
 	
 	var name, race, className;
 });
